e86d927caedf49a8087bccd50d8edc6447fc2d4d
javjuarez <sonico@ciencias.unam.mx>
2022-02-25 23:23:12 -0600

    - Splash Screen creado TODO - Quitar hard-coding

---
+++ app/src/main/java/com/example/formulas/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.example.formulas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, InicioFormulas::class.java))
+            finish()
+        }, 3000)
+    }
+}
